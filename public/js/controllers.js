5c300d8d1bfb72f07e7011a0424b3ee4aa7aa626
Eugene Prystupa <eugene.prystupa@lab49.com>
2013-10-13 18:46:53 -0400

    - add sparkline to grade log

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,10 +8,19 @@
 function GradebookController($scope, $filter) {
 
     var sock;
+    var history;
+
+    function updateHistory(grades) {
+        grades.forEach(function (grade) {
+            if (!history[grade.course]) history[grade.course] = [];
+            history[grade.course].push(grade.avg);
+        });
+    }
 
     (function createSocket() {
         sock = new SockJS('/services/rt/gradebook');
         sock.onopen = function () {
+            history = {};
             console.log('opened gradebook socket');
         };
 
@@ -20,6 +29,13 @@ function GradebookController($scope, $filter) {
             $scope.updated = $filter('date')(data.timestamp, 'short');
             $scope.grades = data.grades;
             $scope.$apply();
+
+            updateHistory(data.grades);
+
+            $('.sparkline').each(function (index, el) {
+                var values = history[$(el).attr('data-course')];
+                $(el).sparkline(values, {width: '70px', normalRangeMin: 90, normalRangeMax: 100});
+            });
         };
 
         sock.onclose = function () {
